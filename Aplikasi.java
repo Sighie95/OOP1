b53184f78fd8a3a48aee8e63168fe43b28cc7981
Sighie95 <48244663+Sighie95@users.noreply.github.com>
2019-03-31 22:50:58 +0700

    Create Aplikasi.java

---
+++ Aplikasi.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Aplikasi {
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame("halo");
+        frame.setSize(300, 200);
+
+        JLabel lblNama = new JLabel();
+        lblNama.setText("Yogi");
+        frame.add(lblNama, BorderLayout.WEST);
+        JLabel lblNim = new JLabel();
+        lblNim.setText("17090007");
+
+        JButton btnSimpan = new JButton("Simpan");
+        frame.add(btnSimpan, BorderLayout.EAST);
+
+        JPanel westPanel = new JPanel();
+        westPanel.setLayout(new GridLayout(3,1));
+        frame.add(westPanel, BorderLayout.WEST);
+        westPanel.add(lblNama);
+        westPanel.add(lblNim);
+
+        frame.setVisible(true);
+
+
+    }
+}
